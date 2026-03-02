ed28c35fcf5dee3c132c531a5ce9fbba58d5e25c
时雨 <270598250@qq.com>
2026-03-02 23:35:53 +0800

    编纂UI部分项目结构

---
+++ better-agent/ui/gui/macOS/better-agent/better-agent/better_agentApp.swift
@@ -0,0 +1,17 @@
+//
+//  better_agentApp.swift
+//  better-agent
+//
+//  Created by 时雨 on 2026/3/2.
+//
+
+import SwiftUI
+
+@main
+struct better_agentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
