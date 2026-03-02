fa9a23ab563e70236f07227f220bc005140c994c
时雨 <270598250@qq.com>
2026-03-02 23:35:53 +0800

    编纂UI部分项目结构

---
+++ better-agent/ui/gui/macOS/better-agent/better-agentTests/better_agentTests.swift
@@ -0,0 +1,17 @@
+//
+//  better_agentTests.swift
+//  better-agentTests
+//
+//  Created by 时雨 on 2026/3/2.
+//
+
+import Testing
+@testable import better_agent
+
+struct better_agentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
